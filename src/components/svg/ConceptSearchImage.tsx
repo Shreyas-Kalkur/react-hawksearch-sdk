3527ec60d3c81bea767bacc9122122e388864126
Shreyas <skalkur@bridgeline.com>
2024-12-11 12:38:06 +0530

    concept search

---
+++ src/components/svg/ConceptSearchImage.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ConceptSearchIcon: React.FC = () => (
+  <svg
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    width="40"
+    height="40"
+    viewBox="0 0 331 302"
+  >
+    <path
+      d="M0 0 C109.23 0 218.46 0 331 0 C331 99.66 331 199.32 331 302 C221.77 302 112.54 302 0 302 C0 202.34 0 102.68 0 0 Z"
+      fill="#FEFEFE"
+      transform="translate(0,0)"
+    />
+    <path
+      d="M0 0 C2.40805934 0.94862944 4.1335164 2.09019321 6.0625 3.8125 C6.0625 4.4725 6.0625 5.1325 6.0625 5.8125 C4.0825 5.3175 4.0825 5.3175 2.0625 4.8125 C1.7325 17.6825 1.4025 30.5525 1.0625 43.8125 C7.3325"
+      fill="#000000"
+    />
+    {/* Add additional paths or SVG elements as needed */}
+  </svg>
+);
+
+export default ConceptSearchIcon;
